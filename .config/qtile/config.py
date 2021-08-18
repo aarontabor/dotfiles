90eb4b0eb856dc709e4f354d86fa5e28c369f1cb
Aaron Tabor <aarontabor50@gmail.com>
2021-08-18 17:45:04 -0300

    qtile. Modify configuration for column-based workflow
    3 Screen
      Mod-<7,8,9> to focus screen
    
    Each screen uses column-based layout.
      Mod-<N> for N-columns (1-4)
      Mod-Return to collapse / stack focused column
    
    Mod-<h,j,k,l> for change focus between windows
    
    Mod-m to enter 'move' mode
      - h,j,k,l to move windows
    
    Mod-r to enter 'resize' mode
      - h,j,k,l to resize focused window
    
    Pneumonic groups (e.g. "m"ain, "c"hat)
      Mod-g + Mod-<group-key> to move to a group
      Mod-shift-g + Mod-shift-<group-key> to move window to a group

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -1,9 +1,8 @@
-from typing import List  # noqa: F401
+from typing import List
 
 from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Screen
+from libqtile.config import Click, Drag, Group, Key, KeyChord, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
 
 mod = "mod4"
 alt = "mod1"
@@ -11,90 +10,86 @@ alt = "mod1"
 terminal = "alacritty"
 
 
+groups = [Group(i) for i in "mldcrx"]
+
 keys = [
-    # Switch between windows in current stack pane
-    Key([mod], "k", lazy.layout.down(), desc="Move focus down in stack pane"),
-    Key([mod], "j", lazy.layout.up(), desc="Move focus up in stack pane"),
-
-    # Move windows up or down in current stack
-    Key([mod, "shift"], "k", lazy.layout.shuffle_down(), desc="Move window down in current stack "),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_up(), desc="Move window up in current stack "),
-
-    # Resize windows in Mondad-* views
-    Key([mod], "equal", lazy.layout.grow(), desc="Increase size of current window"),
-    Key([mod], "minus", lazy.layout.shrink(), desc="Decrease size of current window"),
-    Key([mod, "shift"], "equal", lazy.layout.grow_main(), desc="Increase size of master window"),
-    Key([mod, "shift"], "minus", lazy.layout.shrink_main(), desc="Decrease size of master window"),
-    Key([mod], "0", lazy.layout.normalize(), desc="Reset secondary window sizes"),
-
-    # Swap current window with master in Monad views
-    Key([mod], "Return", lazy.layout.swap_main(), desc="Swap current window with master"),
-
-    # Flip master region in Monad views (e.g., from left to right)
-    Key([mod, "shift"], "Return", lazy.layout.flip(), desc="Flip the master region"),
-
 
     # Moving between (Physical) Screens
-    Key([mod], "h", lazy.to_screen(0), desc="Move to Left Screen"),
-    Key([mod], "l", lazy.to_screen(1), desc="Move to Right Screen"),
+    ## TODO: How does this ordering get assigned? Is this based on the order I specify in xrandr?
+    Key([mod], "7", lazy.to_screen(2), desc="Move to Left Screen"),
+    Key([mod], "8", lazy.to_screen(0), desc="Move to Right Screen"),
+    Key([mod], "9", lazy.to_screen(1), desc="Move to Right Screen"),
+
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus left"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus up"),
+
+    # Move windows
+    KeyChord([mod], "m", [
+        Key([], "h", lazy.layout.shuffle_left(), desc="Move window left"),
+        Key([], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+        Key([], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+        Key([], "l", lazy.layout.shuffle_right(), desc="Move window right"),
+    ], mode="Move Window"),
+
+    # Resize windows
+    KeyChord([mod], "r", [
+        Key([], "h", lazy.layout.grow_left(), desc="Resize window left"),
+        Key([], "j", lazy.layout.grow_down(), desc="Resize window down"),
+        Key([], "k", lazy.layout.grow_up(), desc="Resize window up"),
+        Key([], "l", lazy.layout.grow_right(), desc="Resize window right"),
+    ], mode="Resize Window"),
 
     # Working with Floating Windows
-    Key([mod], "f", lazy.window.toggle_floating(), desc="Toggle Float"),
+    Key([mod], "t", lazy.window.toggle_floating(), desc="Toggle Float"),
     Key([alt], "Tab", lazy.group.next_window(), lazy.window.bring_to_front(), desc="Alt-tab btwn Windows"),
 
+    Key([mod], "1", lazy.to_layout_index(0), desc="Single Column"),
+    Key([mod], "2", lazy.to_layout_index(1), desc="Two Column"),
+    Key([mod], "3", lazy.to_layout_index(2), desc="Three Column"),
+    Key([mod], "4", lazy.to_layout_index(3), desc="Four Column"),
+    Key([mod], "Return", lazy.layout.toggle_split(), desc="Toggle Column Splits"),
+
+    # Group Management
+    KeyChord([mod], "g", [Key([mod], i.name, lazy.group[i.name].toscreen()) for i in groups]),
+    KeyChord([mod, "shift"], "g", [Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=False)) for i in groups]),
+
     # Qtile System Commands
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.restart(), desc="Restart qtile"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown qtile"),
+    Key([mod], "q", lazy.window.kill(), desc="Kill focused window"),
+    Key([mod, "shift"], "r", lazy.restart(), desc="Restart qtile"),
+    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown qtile"),
 
     # Launch Rofi
     Key([mod], "space", lazy.spawn("rofi -show drun"), desc="Launch an Application"),
-
-    # Volume Hotkeys
-    Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle"), desc="Toggle Audio"),
-    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer set Master 1%- unmute"), desc="Decrease Audio"),
-    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer set Master 1%+ unmute"), desc="Increase Audio"),
 ]
-
-
-groups = [Group(i) for i in "123456789"]
-
-for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(), desc="Switch to group {}".format(i.name)),
-
-        # mod1 + shift + letter of group = move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name, switch_group=False), desc="Switch to & move focused window to group {}".format(i.name)),
-    ])
-
 
 layout_theme = {
         'border_width':0,
-        'margin': 15,
-        'fullscreen_border_width':0,
+        'margin': 30,
+        'fair': True,
+        'split': True,
+        'wrap_focus_columns': True,
+        'wrap_focus_rows': True,
+        'wrap_focus_stacks': True,
     }
 
-layouts = [
-    layout.Max(),
-    layout.MonadTall(**layout_theme),
-    layout.MonadWide(**layout_theme),
-]
+layouts = [layout.Columns(num_columns=i, **layout_theme) for i in range(1,5)]
 
 
-screens = [
-    Screen(
-        wallpaper='~/.background.jpg',
-        wallpaper_mode='fill',
-        # bottom=bar.Bar([ widget.CurrentLayout(), ], 30),
-    ),
-    Screen(
-        wallpaper='~/.background.jpg',
-        wallpaper_mode='fill',
-        # bottom=bar.Bar([ widget.CurrentLayout(), ], 30),
-    ),
-]
+screen_settings = {
+        'wallpaper': '~/.background.jpg',
+        'wallpaper_mode': 'fill',
+    }
+
+def make_bar():
+    return bar.Bar([
+        widget.GroupBox(),
+        widget.Chord(),
+    ], 30)
+
+screens = [Screen(**screen_settings, top=make_bar()) for _i in range(3)]
 
 
 mouse = [
@@ -107,7 +102,7 @@ mouse = [
 
 
 dgroups_key_binder = None
-dgroups_app_rules = []  # type: List
+dgroups_app_rules = []  # type: ListR
 main = None  # WARNING: this is deprecated and will be removed soon
 follow_mouse_focus = False
 bring_front_click = False
@@ -131,4 +126,4 @@ floating_layout = layout.Floating(float_rules=[
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
-wmname = "QTile"
+wmname = "QTileMaybe use key-sequence for this - for example, <mod>-g + a"
